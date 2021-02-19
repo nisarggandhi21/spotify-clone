3798bfc89ae7246a589da4b0299a37fa348fd487
Nisarg Gandhi <nisarggandhi21@gmail.com>
2021-02-20 00:02:20 +0530

    v1

---
+++ build/precache-manifest.ce32ad0b84e6bb0181d94d0d23ef18f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3483a7a3e8839587a1f0f3dbd5bdceda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "904de165d32c99df1c44",
+    "url": "/static/css/main.93214008.chunk.css"
+  },
+  {
+    "revision": "fbf539408ac4a93e2565",
+    "url": "/static/js/2.eb866343.chunk.js"
+  },
+  {
+    "revision": "277f9ac65ea629533c781a2e9dd27b69",
+    "url": "/static/js/2.eb866343.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "904de165d32c99df1c44",
+    "url": "/static/js/main.fe6c4993.chunk.js"
+  },
+  {
+    "revision": "6ca49defa095afd3e9c1",
+    "url": "/static/js/runtime-main.f9911b94.js"
+  }
+]);
